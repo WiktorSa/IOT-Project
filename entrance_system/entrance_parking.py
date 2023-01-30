47c932eeda78a9b37cc0000166d3b8b0df1959e7
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-01-30 20:20:22 +0100

    Add text

--- entrance_system/entrance_parking.py
+++ entrance_system/entrance_parking.py
@@ -121,6 +121,7 @@ def read_cards():
                             num_dict[num] = [dt, True]
                             free_parking_places -= 1
                             print(f'Client {num} entered the parking lot.')
+                            print(f'Free places: {free_parking_places} / {total_parking_places}')
                             blink_green()
                             buzzer()
                             stop_blink()
